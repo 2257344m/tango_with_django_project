cfc42fc68f0de75b20918e647da6347f96e9dc45
Caitlin McIntyre <2257344m@student.gla.ac.uk>
2018-01-22 12:14:41 +0000

    Added first HTML template

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About Page</a>')
+    # Creates a dictionary to pass the template engine as its context
+    # Basically will make itself appear where 'boldmessage' is written in the html code
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Returns the rendered response to the client
+    # The first parameter is the template we want to use
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse('Rango says this is the about page. <a href="/rango/">Back</a>.')
