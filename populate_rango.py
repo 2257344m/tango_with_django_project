012816c89ba55287541da657205612b4922bd654
Caitlin McIntyre <2257344m@student.gla.ac.uk>
2018-02-03 17:36:36 +0000

    Created population script

--- populate_rango.py
+++ populate_rango.py
@@ -4,9 +4,72 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
-from rango.model import Category, Page
+from rango.models import Category, Page
 
-def populate()
+def populate():
     # Creating lists of dictionaries, which will contain the pages we wish to
     # add to each category. We will also make a dictionary of dictionaries
     # to contain the categories.
+
+    # Page dictionaries
+
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url": "http://docs.python.org/2/tutorial/"},
+        {"title": "How to Think like a Computer Scientist",
+         "url": "http://www.greenteapress.com/thinkpython/"},
+        {"title": "Learn Python in 10 Minutes",
+         "url": "http://www.korokithakis.net/tutorials/python/"}]
+
+    django_pages = [
+        {"title": "Official Django Tutorial",
+         "url": "http://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        {"title": "Django Rocks",
+         "url": "https://www.djangorocks.com/"},
+        {"title": "How to Tango with Django",
+         "url": "http://www.tangowithdjango.com/"}
+        ]
+
+    other_pages = [
+        {"title": "Bottle",
+         "url": "http://bottlepy.org/docs/dev/"},
+        {"title": "Flask",
+         "url": "http://flask.pocoo.org"}
+        ]
+
+    # Category dictionary
+    
+    cats = {"Python": {"pages": python_pages},
+            "Django": {"pages": django_pages},
+            "Other Frameworks": {"pages": other_pages}}
+
+    # Iteration to use the dictionaries to create the pages/categories
+
+    for cat, cat_data in cats.items():
+            c = add_cat(cat)
+            for p in cat_data["pages"]:
+                add_page(c, p["title"], p["url"])
+
+    # Printing those added categories
+
+    for c in Category.objects.all():
+            for p in Page.objects.filter(category=c):
+                print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+            p = Page.objects.get_or_create(category=cat, title=title)[0]
+            p.url=url
+            p.views=views
+            p.save()
+            return p
+
+def add_cat(name):
+            c = Category.objects.get_or_create(name=name)[0]
+            c.save()
+            return c
+
+# Actual script execution
+
+if __name__ == '__main__':
+    print ("Starting Rango population script...")
+    populate()
